78596ac03b9c9772c20d0684c844e1b943b78b37
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-27 10:36:03 +0900

    Initial Commit

---
+++ ImageView Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageView Timer
+//
+//  Created by amadeus on 27/03/2019.
+//  Copyright © 2019 DIT Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
